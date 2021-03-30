facbb94ff66e6349df3f7df7497eeb861ad11b4d
dsoto06 <diegosr06@hotmail.com>
2021-03-30 02:59:29 +0000

    Primera prueba

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, To_do
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,55 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/get_fav', methods=['GET'])
+def get_fav():
 
-    return jsonify(response_body), 200
+    # get all the people
+    query = Favorites.query.all()
+
+    # map the results and your list of people  inside of the all_people variable
+    all_favs = list(map(lambda x: x.serialize(), query))
+
+    return jsonify(all_favs), 200
+
+@app.route('/add_fav', methods=['POST'])
+def add_fav():
+
+    request_body = request.get_json()
+    fav = Favorites(name=request_body["name"])
+    db.session.add(fav)
+    db.session.commit()
+
+    return jsonify("Favorito agregado de forma correcta."), 200
+
+@app.route('/upd_fav/<int:fid>', methods=['PUT'])
+def upd_fav(fid):
+
+    fav = Favorites.query.get(fid)
+    if fav is None:
+        raise APIException('Favorite not found', status_code=404)
+
+    request_body = request.get_json()
+
+    if "name" in request_body:
+        fav.name = request_body["name"]
+
+    db.session.commit()
+    return jsonify("Favorito modificado de forma correcta."), 200
+
+@app.route('/del_fav/<int:fid>', methods=['DELETE'])
+def del_fav(fid):
+
+    fav = Favorites.query.get(fid)
+
+    if fav is None:
+        raise APIException('Favorite not found', status_code=404)
+    db.session.delete(fav)
+    db.session.commit()
+
+    return jsonify("Favorito eliminado de forma correcta."), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
